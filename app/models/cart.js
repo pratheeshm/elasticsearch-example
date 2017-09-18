84f18f1fa4ccb676476af178efa9246a876b0650
Pratheesh M <pratheesh@qburst.com>
2017-09-18 11:41:59 +0530

    add required property in cart schema

--- app/models/cart.js
+++ app/models/cart.js
@@ -2,17 +2,20 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 var productSchema=new Schema({
     productID:{
-        type:String
+        type:String,
+        required:true
     },
     name:{
-        type:String
+        type:String,
+        required:true
     },
     price:{
         type:Number,
         required:true
     },
     quantity:{
-        type:Number
+        type:Number,
+        required:true
     }
 },{ versionKey: false });
 const Cart=module.exports=mongoose.model("Model",productSchema,"cart");
