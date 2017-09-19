d2f02f75d46a98c5c1eb74368cc948cbf989ccc2
Pratheesh M <pratheesh@qburst.com>
2017-09-19 14:20:56 +0530

    included decrementing product quantity

--- app/services/cart.js
+++ app/services/cart.js
@@ -1,5 +1,5 @@
 const Cart=require('../models/cart');
-
+const Product=require('../models/product');
 module.exports.addCart = function(product, callback){
     Cart.find({productID:product.productID},(err,result)=>{
         if(err){
@@ -7,6 +7,11 @@ module.exports.addCart = function(product, callback){
             return;
         }
         if(result.length!=0){
+
+            Product.update({productID:product.productID},{$inc:{quantity:(-1)}},function(err,result){
+                console.log("product==>",result);
+            });
+
             Cart.findOneAndUpdate({productID:product.productID},{quantity:(result[0].quantity+product.quantity)},callback);
         }
         else if(result.length==0){
