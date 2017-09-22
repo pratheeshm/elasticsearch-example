7a13e164a0fdfb447b0d16ec4ea5d51a0bf5e05a
Pratheesh M <pratheesh@qburst.com>
2017-09-22 09:28:25 +0530

    structured folders

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var express=require('express');
 var bodyParser = require('body-parser');
-var server=require('./config/server');
-var routes=require('./app/routes/index');
+var server=require('./app/config/server');
+var routes=require('./app/client/routes/index');
 var app=express();
 app.use(bodyParser.json());
 app.use(function(req,res,next){
@@ -9,5 +9,5 @@ app.use(function(req,res,next){
 res.header("Access-Control-Allow-Headers", "origin, content-type, accept, authorization");
 next();})
 routes(app);
-require('./config/connect.js');
+require('./app/config/connect.js');
 app.listen(server.port);
